fbd5cd870d3008a1da5b2c0294cb4d8008dce29b
Dawid Ciężarkiewicz <dpc@ucore.info>
2010-07-28 22:12:19 +0200

    Fixes.

--- 3rd/install.sh
+++ 3rd/install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # basic 3rd party tools installator (or a instruction)
 
-# Mardown for PHP (with extras)
+# Markdown for PHP (with extras)
 wget http://michelf.com/docs/projets/php-markdown-extra-1.2.4.zip
-ln -sf "PHP Markdown*/markdown.php" ./
+unzip php-markdown-extra-1.2.4.zip
+ln -sf */markdown.php ./
 
